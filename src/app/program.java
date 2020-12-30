84e1d5b1cc15afb44f2588339db1214ff4d87b34
Vinicius Trigo <viniciustrigo52@gmail.com>
2020-12-29 21:34:41 -0300

    Department enttity

---
+++ src/app/program.java
@@ -0,0 +1,14 @@
+package app;
+
+import model.entities.Department;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Department obj = new Department(1, "books");
+		System.out.println(obj);
+	}
+
+}
